7858ee016b7d3ffee447aee04de5bdb5b3bc1537
Alex Gerdisch <alexgerdisch@gmail.com>
2021-05-18 14:55:38 -0700

    Built Timer to track total meditation time

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 let counter = document.getElementById("counter");
 let instruction = document.getElementById("instruction");
 let cycleCount = 1;
-
+let globalCount = document.getElementById("total");
+let totalCount = 1;
 
 
 function updateCounter() {
     let tempCount = parseInt(counter.innerHTML, 10);
+    totalCount++;
     
-    // Every 4 seconds, change instruction
+    // Changing Instruction text
     if (cycleCount < 4) {
         instruction.innerHTML=`Inhale`;
-        
         counter.style.transform = "scale(1.5)";
         instruction.style.transform = "scale(1.5)";
         cycleCount++;
@@ -19,7 +20,6 @@ function updateCounter() {
         cycleCount++;
     } else if (cycleCount >= 8 && cycleCount < 12) {
         instruction.innerHTML=`Exhale`;
-        
         instruction.style.transform = "scale(1)";
         counter.style.transform = "scale(1)";
         cycleCount++;
@@ -33,17 +33,35 @@ function updateCounter() {
         counter.style.transform = "scale(1.5)";
     }
 
+    // Updating Main Counter
     if (tempCount < 4) {
         tempCount++;
     } else if (tempCount == 4) {
         tempCount = 1;
     }
 
-    document.getElementById("counter").innerHTML=`${tempCount}`;
-    return tempCount;
+    // Updating Total Meditation Time
+    
+   numMinutes = Math.floor(totalCount / 60);
+   numSeconds = totalCount % 60;
+   
+
+    if (numMinutes >= 1 && numMinutes < 10 && numSeconds < 10) {
+        globalCount.innerHTML= `0${numMinutes}:0${numSeconds}`;
+    } else if (numMinutes >= 1 && numMinutes < 10 && numSeconds >= 10) {
+        globalCount.innerHTML= `0${numMinutes}:${numSeconds}`;
+    } else if (numMinutes >= 10 && numSeconds < 10) {
+        globalCount.innerHTML= `${numMinutes}:0${numSeconds}`;
+    } else if (numMinutes == 0 && numSeconds < 10) {
+        globalCount.innerHTML = `00:0${numSeconds}`;
+    } else if (numMinutes == 0 && numSeconds >= 10) {
+        globalCount.innerHTML = `00:${numSeconds}`;
+    }
+
+    counter.innerHTML=`${tempCount}`;
+
 }
 
 
-
+// Starting the Meditation
 setInterval(updateCounter, 1000);
-// clearInterval();s
